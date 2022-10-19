3c9dd3631ea124c0e914d32566526883a6e6cb92
cchanmi <gkfn980@hs.ac.kr>
2022-10-19 21:12:52 +0900

    [Add] #1 - 프로젝트 생성

---
+++ Mukjipsa-iOS/Mukjipsa-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mukjipsa-iOS
+//
+//  Created by 황찬미 on 2022/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
