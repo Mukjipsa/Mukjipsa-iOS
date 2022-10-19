430a8df6b1d4c51fe9cbf0d1ecff24f7f3aa25d2
cchanmi <gkfn980@hs.ac.kr>
2022-10-19 21:30:49 +0900

    [Chore] #1 - 폴더링

---
+++ Mukjipsa-iOS/Mukjipsa-iOS/Global/Constant/Constant.swift
@@ -0,0 +1,8 @@
+//
+//  Constant.swift
+//  Mukjipsa-iOS
+//
+//  Created by 황찬미 on 2022/10/19.
+//
+
+import Foundation
---
+++ Mukjipsa-iOS/Mukjipsa-iOS/Screen/Screen.swift
@@ -0,0 +1,8 @@
+//
+//  Screen.swift
+//  Mukjipsa-iOS
+//
+//  Created by 황찬미 on 2022/10/19.
+//
+
+import Foundation
