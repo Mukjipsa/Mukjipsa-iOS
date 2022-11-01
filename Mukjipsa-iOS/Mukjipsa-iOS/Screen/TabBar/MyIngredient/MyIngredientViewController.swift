a3a499953dca2db79540eb5607a03581d0339739
cchanmi <gkfn980@hs.ac.kr>
2022-11-01 19:33:37 +0900

    [Setting] #7 - TabBar 구현

---
+++ Mukjipsa-iOS/Mukjipsa-iOS/Screen/TabBar/MyIngredient/MyIngredientViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MyIngredientViewController.swift
+//  Mukjipsa-iOS
+//
+//  Created by 황찬미 on 2022/11/01.
+//
+
+import UIKit
+
+final class MyIngredientViewController: UIViewController {
+    
+    // MARK: - Property
+    
+    // MARK: - UI Property
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - @objc
+    
+    // MARK: - Custom Method
+
+}
---
+++ Mukjipsa-iOS/Mukjipsa-iOS/Screen/TabBar/Recipe/RecipeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RecipeViewController.swift
+//  Mukjipsa-iOS
+//
+//  Created by 황찬미 on 2022/11/01.
+//
+
+import UIKit
+
+final class RecipeViewController: UIViewController {
+    
+    // MARK: - Property
+    
+    // MARK: - UI Property
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: - @objc
+    
+    // MARK: - Custom Method
+
+}
