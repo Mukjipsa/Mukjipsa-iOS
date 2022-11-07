ff031ae2d9f2bd2e09c24ff872e43c4734aaa511
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-11-07 23:28:23 +0900

    [Chore] #14 - setletterSpacingAndlineHeight 샘플코드

--- Mukjipsa-iOS/Mukjipsa-iOS/Screen/TabBar/Recipe/RecipeViewController.swift
+++ Mukjipsa-iOS/Mukjipsa-iOS/Screen/TabBar/Recipe/RecipeViewController.swift
@@ -6,22 +6,43 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
 final class RecipeViewController: UIViewController {
     
     // MARK: - Property
     
     // MARK: - UI Property
+    private let testLabel = UILabel().then{
+        $0.text = "안녕하세요 테스트 중"
+    }
     
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setUI()
+        setLayout()
+        view.backgroundColor = .white
     }
     
     // MARK: - @objc
     
     // MARK: - Custom Method
+    private func setUI() {
+        testLabel.do {
+            $0.font = .t1
+            $0.setlineHeightAndletterSpacing(letterSpacing: Constant.LetterSpacing.t1)
+        }
+    }
+}
 
+extension RecipeViewController {
+    private func setLayout() {
+        view.addSubview(testLabel)
+        testLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
 }
