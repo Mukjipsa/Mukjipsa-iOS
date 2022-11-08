2806597fbb49f1c78517ea617cdfccfcf45ca106
cchanmi <gkfn980@hs.ac.kr>
2022-11-08 19:59:57 +0900

    [Feat] #13- BJSButton 구현

---
+++ Mukjipsa-iOS/Mukjipsa-iOS/Global/DesignSystem/MJSButton.swift
@@ -0,0 +1,28 @@
+//
+//  MJSButton.swift
+//  Mukjipsa-iOS
+//
+//  Created by 황찬미 on 2022/11/08.
+//
+
+import UIKit
+
+final class MJSButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setButtonStyle()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setButtonStyle() {
+        self.backgroundColor = .primary
+        self.setTitleColor(.white, for: .normal)
+        self.titleLabel?.font = .bu1
+        self.layer.cornerRadius = 16
+        self.titleLabel?.textAlignment = .center
+    }
+}
