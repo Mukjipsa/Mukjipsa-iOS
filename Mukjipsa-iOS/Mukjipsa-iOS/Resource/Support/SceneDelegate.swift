5a6886c8b64f0c20187d6208cf3f71f07964d667
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-11-09 19:46:38 +0900

    [Chore] #20 - 디자인 관련 파일 변경 1. UIColor와 UIFont 변경사항 반영 2. String 리터럴 추가 3. font Info.plist 추가

--- Mukjipsa-iOS/Mukjipsa-iOS/Resource/Support/SceneDelegate.swift
+++ Mukjipsa-iOS/Mukjipsa-iOS/Resource/Support/SceneDelegate.swift
@@ -15,7 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: scene)
-        self.window?.rootViewController = TabBarController()
+//        self.window?.rootViewController = TabBarController()
+        self.window?.rootViewController = BasicIngredientViewController()
         self.window?.makeKeyAndVisible()
     }
 
