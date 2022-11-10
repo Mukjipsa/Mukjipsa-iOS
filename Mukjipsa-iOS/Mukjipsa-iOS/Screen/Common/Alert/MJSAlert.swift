7ec918cf1a484a8610b731c8d0b5695a8992f31e
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-11-10 17:33:39 +0900

    [Add] #33 - 취소 커스텀 버튼 구현

---
+++ Mukjipsa-iOS/Mukjipsa-iOS/Screen/Common/Alert/MJSAlert.swift
@@ -0,0 +1,20 @@
+//
+//  MJSAlert.swift
+//  Mukjipsa-iOS
+//
+//  Created by 강윤서 on 2022/11/10.
+//
+
+import UIKit
+
+class MJSAlert: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
