8a0188bf4a46d971d62fd0eaf5df167d0404b7b5
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-11-10 11:28:05 +0900

    [Chore] #28 - 폴더링 변경

---
+++ Mukjipsa-iOS/Mukjipsa-iOS/Screen/Onboarding/Guide/View/GuideView.swift
@@ -0,0 +1,20 @@
+//
+//  GuideView.swift
+//  Mukjipsa-iOS
+//
+//  Created by 강윤서 on 2022/11/10.
+//
+
+import UIKit
+
+class GuideView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
