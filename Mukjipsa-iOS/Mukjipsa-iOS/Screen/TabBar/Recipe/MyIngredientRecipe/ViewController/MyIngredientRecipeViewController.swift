bc304ec17983465c57b5c85989db6ec56abd36c1
cchanmi <gkfn980@hs.ac.kr>
2022-11-11 04:47:54 +0900

    [Design] #30 - 재료 기반 레시피 TableView UI

---
+++ Mukjipsa-iOS/Mukjipsa-iOS/Screen/TabBar/Recipe/MyIngredientRecipe/ViewController/MyIngredientRecipeViewController.swift
@@ -0,0 +1,83 @@
+//
+//  RecipeViewController.swift
+//  Mukjipsa-iOS
+//
+//  Created by 황찬미 on 2022/11/01.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class MyRecipeViewController: BaseViewController {
+    
+    // MARK: - Property
+    
+    private let mainNaviView = RecipeNaviView(frame: CGRect(), mode: .mainRecipe)
+    private lazy var recipeTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.separatorStyle = .none
+        tableView.showsVerticalScrollIndicator = false
+        return tableView
+    }()
+    
+    // MARK: - UI Property
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setLayout()
+        registerCell()
+        setDelegate()
+    }
+    
+    // MARK: - @objc
+    
+    // MARK: - Custom Method
+    private func setLayout() {
+        view.addSubviews([mainNaviView, recipeTableView])
+        
+        mainNaviView.snp.makeConstraints {
+            $0.leading.top.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(44)
+        }
+        
+        recipeTableView.snp.makeConstraints {
+            $0.top.equalTo(mainNaviView.snp.bottom)
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.bottom.equalToSuperview()
+        }
+    }
+    
+    private func registerCell() {
+        RecipeTableViewCell.register(target: recipeTableView)
+    }
+
+    private func setDelegate() {
+        recipeTableView.delegate = self
+        recipeTableView.dataSource = self
+    }
+    
+    override func bind() {}
+}
+
+extension MyRecipeViewController: UITableViewDelegate {
+}
+
+extension MyRecipeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let recipeCell = tableView.dequeueReusableCell(withIdentifier: RecipeTableViewCell.className, for: indexPath) as? RecipeTableViewCell else { return UITableViewCell() }
+        return recipeCell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 355
+    }
+}
