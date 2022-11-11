bf62bedfe0b79208ce1cd494d6a22ccba470050a
cchanmi <gkfn980@hs.ac.kr>
2022-11-12 01:23:02 +0900

    [Design] #30 - 레시피 TableView Cell 그림자

--- Mukjipsa-iOS/Mukjipsa-iOS/Screen/TabBar/Recipe/MyIngredientRecipe/Cell/RecipeTableViewCell.swift
+++ Mukjipsa-iOS/Mukjipsa-iOS/Screen/TabBar/Recipe/MyIngredientRecipe/Cell/RecipeTableViewCell.swift
@@ -12,14 +12,16 @@ import Then
 
 final class RecipeTableViewCell: UITableViewCell, UITableViewRegisterable {
     
+    // MARK: - Property
+
     static var isFromNib: Bool = false
+    
+    // MARK: - UI Property
     
     private let recipeImage = UIView().then {
         $0.backgroundColor = .green
     }
-    private let recipeContainerView = UIView().then {
-        $0.backgroundColor = .primary
-    }
+    private let recipeContainerView = UIView()
     private let recipeName = UILabel().then {
         $0.text = "수제버거"
         $0.font = .h1
@@ -44,6 +46,8 @@ final class RecipeTableViewCell: UITableViewCell, UITableViewRegisterable {
         $0.setLineHeightAndletterSpacing(letterSpacing: Constant.LetterSpacing.b4)
     }
     
+    // MARK: - Life Cycle
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setLayout()
@@ -55,27 +59,34 @@ final class RecipeTableViewCell: UITableViewCell, UITableViewRegisterable {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0.0, left: 0.0, bottom: 16.0, right: 0.0))
+    }
+    
+    // MARK: - Custom Method
+    
     private func setCellEffect() {
-        self.contentView.layer.cornerRadius = 20
-        self.contentView.layer.masksToBounds = true
+        contentView.layer.cornerRadius = 20
+        contentView.layer.borderWidth = 0.01
+        contentView.layer.applySketchShadow(color: .black, alpha: 0.1, x: 0.0, y: 0.0, blur: 10.0, spread: 0.0)
     }
     
     private func setLayout() {
-        contentView.addSubviews([recipeImage, recipeContainerView])
-        recipeContainerView.addSubviews([recipeName, recipeIc, channelName, channelSite])
+        contentView.addSubview(recipeContainerView)
+        recipeContainerView.addSubviews([recipeImage, recipeName, recipeIc, channelName, channelSite])
+        
+        recipeContainerView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
         
         recipeImage.snp.makeConstraints {
             $0.leading.top.trailing.equalToSuperview()
             $0.height.equalTo(201)
         }
         
-        recipeContainerView.snp.makeConstraints {
-            $0.top.equalTo(recipeImage.snp.bottom)
-            $0.leading.bottom.trailing.equalToSuperview()
-        }
-        
         recipeName.snp.makeConstraints {
-            $0.top.equalTo(recipeContainerView.snp.top)
+            $0.top.equalTo(recipeImage.snp.bottom).offset(16)
             $0.leading.equalTo(recipeContainerView.snp.leading).inset(16)
         }
         
@@ -83,7 +94,6 @@ final class RecipeTableViewCell: UITableViewCell, UITableViewRegisterable {
             $0.top.equalTo(recipeName.snp.bottom).offset(3)
             $0.leading.equalTo(recipeContainerView.snp.leading).inset(16)
             $0.width.height.equalTo(18)
-            $0.bottom.equalTo(recipeContainerView.snp.bottom).inset(86)
         }
         
         channelName.snp.makeConstraints {
