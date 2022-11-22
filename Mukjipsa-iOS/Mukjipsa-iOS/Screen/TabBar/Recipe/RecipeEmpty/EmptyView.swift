a7d6a31a9a10ce1a913aab8eb5e873885b47db01
cchanmi <gkfn980@hs.ac.kr>
2022-11-22 17:41:51 +0900

    [Chore] #50 - enum 코드 수정

--- Mukjipsa-iOS/Mukjipsa-iOS/Screen/TabBar/Recipe/RecipeEmpty/EmptyView.swift
+++ Mukjipsa-iOS/Mukjipsa-iOS/Screen/TabBar/Recipe/RecipeEmpty/EmptyView.swift
@@ -12,15 +12,20 @@ import Then
 
 final class EmptyView: UIView {
     
-    // 1. 레시피 empty
-    // 2. 검색 empty - 1, - 2
-    // 3. 북마크 empty
-    
-    enum emptyView {
+    enum EmptyView {
         case recipeEmpty
         case searchEmpty
         case bookmarkEmpty
     }
+    
+    var emptyView: EmptyView = .recipeEmpty {
+        didSet {
+            setUI()
+            setLayout()
+        }
+    }
+    
+    // MARK: - UIComponents
     
     let recipeNumberLabel = UILabel().then {
         $0.text = "레시피 0개"
@@ -55,9 +60,10 @@ final class EmptyView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func setUI(mode: emptyView) {
-        switch mode {
-        case .recipeEmpty:
+    // MARK: - func
+    
+    func setUI() {
+        if emptyView == .recipeEmpty {
             emptyImage.do {
                 $0.backgroundColor = .dark
             }
@@ -78,30 +84,28 @@ final class EmptyView: UIView {
                 $0.setLineHeightAndletterSpacing(Constant.fontSize.b2, Constant.LetterSpacing.b2)
                 $0.textAlignment = .center
             }
-        case .bookmarkEmpty:
-            possibleButton.isHidden = true
+        } else if emptyView == .bookmarkEmpty {
             emptyImage.do {
-                $0.backgroundColor = .primary
+                $0.backgroundColor = .dark
             }
             headerLabel.do {
-                $0.text = "레시피를 북마크 해보세요!"
-                $0.font = .sh2
+                $0.text = "입력하지 않은 재료가 있나요?"
+                $0.font = .h3
                 $0.textColor = .dark
-                $0.setLineHeightAndletterSpacing(Constant.fontSize.sh2, Constant.LetterSpacing.sh2)
+                $0.setLineHeightAndletterSpacing(Constant.fontSize.h3, Constant.LetterSpacing.h3)
                 $0.textAlignment = .center
             }
             subLabel.do {
                 $0.text = """
-                          마음에 드는 레시피를 북마크하면
-                          쉽게 다시 볼 수 있어요
+                          레시피를 추천할 수 있도록
+                          가지고 있는 재료를 모두 입력해 주세요
                           """
                 $0.font = .b2
                 $0.textColor = .gray2
                 $0.setLineHeightAndletterSpacing(Constant.fontSize.b2, Constant.LetterSpacing.b2)
                 $0.textAlignment = .center
             }
-            ingredientSelectedbutton.isHidden = true
-        case .searchEmpty:
+        } else if emptyView == .searchEmpty {
             emptyImage.do {
                 $0.backgroundColor = .blue
             }
@@ -119,11 +123,10 @@ final class EmptyView: UIView {
         }
     }
     
-    func setLayout(mode: emptyView) {
+    func setLayout() {
         addSubviews([recipeNumberLabel, possibleButton, emptyImage, headerLabel, subLabel, ingredientSelectedbutton])
         
-        switch mode {
-        case .recipeEmpty:
+        if emptyView == .recipeEmpty {
             recipeNumberLabel.snp.makeConstraints {
                 $0.top.leading.equalToSuperview().inset(16)
             }
@@ -152,7 +155,7 @@ final class EmptyView: UIView {
                 $0.top.equalTo(subLabel.snp.bottom).offset(40)
                 $0.leading.trailing.equalToSuperview().inset(110)
             }
-        case .bookmarkEmpty:
+        } else if emptyView == .bookmarkEmpty {
             recipeNumberLabel.snp.makeConstraints {
                 $0.top.leading.equalToSuperview().inset(16)
             }
@@ -172,25 +175,25 @@ final class EmptyView: UIView {
                 $0.top.equalTo(headerLabel.snp.bottom).offset(16)
                 $0.centerX.equalToSuperview()
             }
-        case .searchEmpty:
-            recipeNumberLabel.snp.makeConstraints {
-                $0.top.leading.equalToSuperview().inset(16)
-            }
-            
-            possibleButton.snp.makeConstraints {
-                $0.top.trailing.equalToSuperview().inset(16)
-            }
-            
-            emptyImage.snp.makeConstraints {
-                $0.top.equalTo(recipeNumberLabel.snp.bottom).offset(132)
-                $0.width.equalTo(358)
-                $0.height.equalTo(200)
-            }
-            
-            subLabel.snp.makeConstraints {
-                $0.top.equalTo(emptyImage.snp.bottom).offset(68)
-                $0.centerX.equalToSuperview()
-            }
+        } else if emptyView == .searchEmpty {
+                recipeNumberLabel.snp.makeConstraints {
+                    $0.top.leading.equalToSuperview().inset(16)
+                }
+                
+                possibleButton.snp.makeConstraints {
+                    $0.top.trailing.equalToSuperview().inset(16)
+                }
+                
+                emptyImage.snp.makeConstraints {
+                    $0.top.equalTo(recipeNumberLabel.snp.bottom).offset(132)
+                    $0.width.equalTo(358)
+                    $0.height.equalTo(200)
+                }
+                
+                subLabel.snp.makeConstraints {
+                    $0.top.equalTo(emptyImage.snp.bottom).offset(68)
+                    $0.centerX.equalToSuperview()
+                }
         }
     }
 }
