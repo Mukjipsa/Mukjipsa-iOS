7129178e244be6c535b7c49fe576bda59ae0a499
cchanmi <gkfn980@hs.ac.kr>
2022-11-24 20:58:25 +0900

    [Feat] #50 - TextField left padding 주는 함수 추가

--- Mukjipsa-iOS/Mukjipsa-iOS/Global/Extension/UITextField+.swift
+++ Mukjipsa-iOS/Mukjipsa-iOS/Global/Extension/UITextField+.swift
@@ -25,4 +25,10 @@ extension UITextField {
             self.attributedText = attributedStr
         }
     }
+    
+    func addLeftPadding() {
+      let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: self.frame.height))
+      self.leftView = paddingView
+      self.leftViewMode = ViewMode.always
+    }
 }
